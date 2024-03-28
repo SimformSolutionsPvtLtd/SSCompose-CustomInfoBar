422db1671402433ce5b555daac1587d42959f5af
Aayush Sarikhada <aayush.s@simformsolutions.com>
2024-03-28 14:47:54 +0530

    UNT-T23378 Feat: Implemented Slide In From Top and Bottom animation for JetInfoBar - Also Added SettingDialog to Set duration at runtime by user.

--- app/src/main/java/com/simform/sscustominfobarapp/ui/theme/Type.kt
+++ app/src/main/java/com/simform/sscustominfobarapp/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.simform.sscustominfobar.ui.theme
+package com.simform.sscustominfobarapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
@@ -15,20 +15,4 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
