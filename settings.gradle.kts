603ca1ad5ecb3481c233e09de3462b7794510b4b
Aayush Sarikhada <aayush.s@simformsolutions.com>
2024-07-11 20:31:07 +0530

    UNT-T26997: Add Maven plugin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,11 +11,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
